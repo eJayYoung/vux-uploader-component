69f272f23e128a5ccc916fb049c678ed0fa296bf
eJay <ejay_young@163.com>
2018-11-04 23:01:39 +0800

    test: complement props test

--- test/uploader.spec.js
+++ test/uploader.spec.js
@@ -2,10 +2,32 @@ import { expect } from 'chai'
 import { mount } from '@vue/test-utils'
 import Uploader from '../src/components/uploader';
 
+
+const uploader = mount(Uploader, {
+  propsData: {
+    files: [
+      './assets/pic_160.png'
+    ]
+  }
+});
+
 describe('vux-uploader-component', () => {
-  it('include a type=file input', () => {
-    const uploader = mount(Uploader);
-    expect(uploader.contains('input[type="file"]')).to.be.true;
-  })
+  describe('input', () => {
+    it('has type=file attribute', () => {
+      expect(uploader.find('input').attributes('type')).to.equal('file');
+    });
+  });
+  describe('props', () => {
+    describe('title', () => {
+      it('default to be 图片上传', () => {
+        expect(uploader.find('.vux-uploader_title').text()).to.equal('图片上传');
+      });
+    });
+    describe('files', () => {
+      it('length should be 1', () => {
+        expect(uploader.findAll('.vux-uploader_file').length).to.equal(1);
+      });
+    });
+  });
 });
 
